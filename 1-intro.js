fece5df74a364184cdc6460362108d022b81afe2
AdityaMore7000 <adi.more7000@gmail.com>
2022-12-20 10:52:20 +0530

    formatted code for intro.js

--- 1-intro.js
+++ 1-intro.js
@@ -1,8 +1,7 @@
 const number = 14;
-if(number<10){
-    console.log('Small number!');
-}
-else{
-    console.log("Big number!!");
+if (number < 10) {
+  console.log("Small number!");
+} else {
+  console.log("Big number!!");
 }
 console.log(`This is my first node application`);
